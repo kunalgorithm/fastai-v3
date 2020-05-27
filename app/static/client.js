ba99a1bc478ae888bd8d22ae77cb283ec85cfb5b
shahv1057 <51315913+shahv1057@users.noreply.github.com>
2020-05-27 11:04:20 -0700

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -29,7 +29,7 @@ function analyze() {
   xhr.onload = function(e) {
     if (this.readyState === 4) {
       var response = JSON.parse(e.target.responseText);
-      el("result-label").innerHTML = `It's ${response["result"]}!!`;
+      el("result-label").innerHTML = `${response["result"]} national park 🎊`;
     }
     el("analyze-button").innerHTML = "Analyze";
   };
